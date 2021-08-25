7fc5fe6525935012394b292446fac826ca514726
duck <godqhr2256@gmail.com>
2021-08-25 17:38:30 +0900

    토큰 생성 및 로그인 기능 구현 완료

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name : {
@@ -33,21 +34,43 @@ const userSchema = mongoose.Schema({
     }
 })
 
-userSchema.pre("save",function(next){
+userSchema.pre("save", function(next){
     var user = this;
-    if(user.isModified('password')){ //모델의 비밀번호가 변환 될때만 암호화 함수가 실행되게 설정.
+    if(user.isModified('password')) { //모델의 비밀번호가 변환 될때만 암호화 함수가 실행되게 설정.
     // 비밀번호 암호화
     bcrypt.genSalt(saltRounds, function(err, salt){
         if(err) return next(err) // 에러가 나면 next함수 바로 실행.
+
         bcrypt.hash(user.password, salt, function(err, hash){
             if(err) return next(err)
             user.password = hash
             next()
         })
     })
+  } else {
+      next()
   }
 }); // 유저모델의 정보를 저장하기 전에 함수실행을 위한 코드
 
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // 플레인 패스워드와 암호화 비밀번호를 비교해야함.
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){ // 플레인 패스워드를 bcrypt를 이용하여 암호화 한 후 비교를 하고 콜백함수를 이용한다.
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    //jsonwebtoken을 이용하여 토큰을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 const User = mongoose.model('User',userSchema);
 
 module.exports = {User}
