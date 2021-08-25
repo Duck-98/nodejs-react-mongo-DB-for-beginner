e13d0f38db5bd05a845af61be48dc84dae1cd652
duck <godqhr2256@gmail.com>
2021-08-25 17:38:14 +0900

    ~

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const bodyParser = require('body-parser');
-const {User} = require("./models/User");
-const config = require('./config/key');
-// application / x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+    const express = require('express')
+    const app = express()
+    const port = 4000
+    const bodyParser = require('body-parser');
+    const {User} = require("./models/User");
+    const cookieParser = require('cookie-parser');
+    const config = require('./config/key');
+    // application / x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
 
 // application / json type으로 갖고오기 위한 코드
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 const mongoose = require('mongoose');
 
 mongoose.connect(config.mongoURI,
@@ -34,6 +35,36 @@ app.post('/register', (req, res) => {
         })
     })
 }) //회원가입을 위한 라우터
+
+app.post('/login', (req,res) => {
+// 요청된 이메일이 데이터베이스에 있는지 찾는다.
+
+
+  User.findOne({email: req.body.email} ,(err, user) => {
+      if(!user){
+          return res.json({
+              loginSuccess : false,
+              message : '제공된 이메일에 해당하는 유저가 없습니다.'
+          })
+      }
+// 이메일이 있다면 비밀번호가 맞는지 확인한다.
+      user.comparePassword(req.body.password, (err, isMatch)=>{
+          if(!isMatch) return res.json({loginSuccess : false, message: '비밀번호가 틀렸습니다.'})
+// 비밀번호까지 같으면 token 생성
+          user.generateToken((err, user) => {
+              if(err) return res.status(400).send(err);
+              // 토큰저장
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess : true, userId : user._id })          
+               
+          })
+      })
+  })
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
